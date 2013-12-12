bf3cab017c191bb7b7609a2140e4cc558118ab6e
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2013-12-13 00:37:29 +0200

    Added campus.sdk package

---
+++ eCampus/eCampus.Core/Models/AuthResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCampus.Core.Models
+{
+    public class AuthResult
+    {
+        public string Data { get; set; }
+    }
+}
--- eCampus/eCampus.Core/ViewModels/LoginViewModel.cs
+++ eCampus/eCampus.Core/ViewModels/LoginViewModel.cs
@@ -1,13 +1,9 @@
 ﻿using eCampus.Core.Helpers;
+using eCampus.Core.Models;
 using Microsoft.Phone.Net.NetworkInformation;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
@@ -27,23 +23,12 @@ namespace eCampus.Core.ViewModels
         /// <summary>
         /// Поле команды для обработки события нажатия клавиши "Увiйти"
         /// </summary>
-        private ICommand loginCommand;
+        private ICommand _loginCommand;
 
-        private string login;
+        private BitmapImage _logoSource;
 
-        public string Login
-        {
-            get { return login; }
-            set { login = value; }
-        }
-
-        private string password;
-
-        public string Password
-        {
-            get { return password; }
-            set { password = value; }
-        }
+        public string Login { get; set; }
+        public string Password { get; set; }
 
 
         /// <summary>
@@ -51,19 +36,15 @@ namespace eCampus.Core.ViewModels
         /// </summary>
         public LoginViewModel()
         {
-            this.loginCommand = new DelegateCommand(this.LoginAction);
+            this._loginCommand = new DelegateCommand(this.LoginAction);
         }
-
 
         /// <summary>
         /// Свойство для доступа к команде для обработки события нажатия клавиши "Увiйти"
         /// </summary>
         public ICommand LoginCommand
         {
-            get
-            {
-                return this.loginCommand;
-            }
+            get { return this._loginCommand; }
         }
 
         /// <summary>
@@ -74,12 +55,14 @@ namespace eCampus.Core.ViewModels
         {
             try
             {
-                if (AuthenticationStarted!=null)
+                if (AuthenticationStarted != null)
                 {
                     AuthenticationStarted();
                 }
+
                 AuthResult ar = await CampusAPI.Auth(this.Login, this.Password);
-                if (AuthenticationCompleted!=null)
+
+                if (AuthenticationCompleted != null)
                 {
                     AuthenticationCompleted();
                 }
@@ -94,7 +77,8 @@ namespace eCampus.Core.ViewModels
                 {
                     MessageBox.Show("Відсутній доступ до мережі Інтернет");
                 }
-                if (AuthenticationFailed!=null)
+
+                if (AuthenticationFailed != null)
                 {
                     AuthenticationFailed();
                 }
@@ -105,25 +89,25 @@ namespace eCampus.Core.ViewModels
             }
         }
 
-        private BitmapImage logoSource;
+
         /// <summary>
         /// Получение логотипа в зависимости от темы
         /// </summary>
         public BitmapImage LogoSource
         {
-            get 
+            get
             {
                 Visibility darkBackgroundVisibility = (Visibility)Application.Current.Resources["PhoneDarkThemeVisibility"];
                 if (darkBackgroundVisibility == Visibility.Visible)
-                    logoSource = new BitmapImage() { UriSource = new Uri(@"\Assets\Images\логоwp_чернаятема.png", UriKind.Relative) };
+                    _logoSource = new BitmapImage() { UriSource = new Uri(@"\Assets\Images\логоwp_чернаятема.png", UriKind.Relative) };
                 else
-                    logoSource = new BitmapImage() { UriSource = new Uri(@"\Assets\Images\логоwp_белаятема.png", UriKind.Relative) };
-                return logoSource; 
+                    _logoSource = new BitmapImage() { UriSource = new Uri(@"\Assets\Images\логоwp_белаятема.png", UriKind.Relative) };
+                return _logoSource;
             }
         }
 
 
-        
+
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void RaisePropertyChanged(string propertyName)
