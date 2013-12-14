5538099a5818366555f72ed32aa9a26b47975231
welikoiwanenko <welikoiwanenko@live.com>
2013-12-14 19:38:17 +0200

    Добавление AuthResult

---
+++ eCampus/eCampus.Core/Models/AuthResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace eCampus.Core.Helpers
+{
+    public class AuthResult
+    {
+        public int StatusCode { get; set; }
+        public string TimeStamp { get; set; }
+        public string Guid { get; set; }
+        public object Paging { get; set; }
+        public string Data { get; set; }
+    }
+}
